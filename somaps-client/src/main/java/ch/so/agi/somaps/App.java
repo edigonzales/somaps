ec6db8fa7ebf4efb5033216849570334a6f1a44e
edigonzales <edi.gonzales@gmail.com>
2022-08-01 19:49:36 +0200

    initial commit

---
+++ somaps-client/src/main/java/ch/so/agi/somaps/App.java
@@ -0,0 +1,116 @@
+package ch.so.agi.somaps;
+
+import static elemental2.dom.DomGlobal.console;
+import static elemental2.dom.DomGlobal.fetch;
+import static org.jboss.elemento.Elements.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import org.dominokit.domino.ui.badges.Badge;
+import org.dominokit.domino.ui.breadcrumbs.Breadcrumb;
+import org.dominokit.domino.ui.button.Button;
+import org.dominokit.domino.ui.datatable.ColumnConfig;
+import org.dominokit.domino.ui.datatable.DataTable;
+import org.dominokit.domino.ui.datatable.TableConfig;
+import org.dominokit.domino.ui.datatable.store.LocalListDataStore;
+import org.dominokit.domino.ui.forms.TextBox;
+import org.dominokit.domino.ui.grid.Column;
+import org.dominokit.domino.ui.grid.Row;
+import org.dominokit.domino.ui.icons.Icons;
+import org.dominokit.domino.ui.infoboxes.InfoBox;
+import org.dominokit.domino.ui.modals.ModalDialog;
+import org.dominokit.domino.ui.style.Color;
+import org.dominokit.domino.ui.style.ColorScheme;
+import org.dominokit.domino.ui.tabs.Tab;
+import org.dominokit.domino.ui.tabs.TabsPanel;
+import org.dominokit.domino.ui.themes.Theme;
+import org.dominokit.domino.ui.utils.TextNode;
+
+import com.google.gwt.core.client.GWT;
+import org.gwtproject.safehtml.shared.SafeHtmlUtils;
+
+import com.github.nmorel.gwtjackson.client.ObjectMapper;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.i18n.client.NumberFormat;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.i18n.client.DateTimeFormat;
+
+import elemental2.dom.CSSProperties;
+import elemental2.dom.DomGlobal;
+import elemental2.dom.Event;
+import elemental2.dom.EventListener;
+import elemental2.dom.HTMLDivElement;
+import elemental2.dom.HTMLElement;
+import elemental2.dom.Location;
+import ol.AtPixelOptions;
+import ol.Extent;
+import ol.Map;
+import ol.MapBrowserEvent;
+import ol.OLFactory;
+import ol.Pixel;
+import ol.events.condition.Condition;
+import ol.Feature;
+import ol.FeatureAtPixelFunction;
+import ol.format.GeoJson;
+import ol.interaction.Select;
+import ol.interaction.SelectOptions;
+import ol.layer.Base;
+import ol.layer.Layer;
+import ol.layer.VectorLayerOptions;
+import ol.proj.Projection;
+import ol.proj.ProjectionOptions;
+import ol.source.Vector;
+import ol.source.VectorOptions;
+import ol.style.Fill;
+import ol.style.Stroke;
+import ol.style.Style;
+import proj4.Proj4;
+
+public class App implements EntryPoint {
+
+    // Application settings
+    private String myVar;
+
+    // Format settings
+    private NumberFormat fmtDefault = NumberFormat.getDecimalFormat();
+    private NumberFormat fmtPercent = NumberFormat.getFormat("#0.0");
+
+    // Projection
+    //private static final String EPSG_2056 = "EPSG:2056";
+    //private static final String EPSG_4326 = "EPSG:4326"; 
+    //private Projection projection;
+
+    private String MAP_DIV_ID = "map";
+    private Map map;
+
+    public void onModuleLoad() {
+        init();
+    }
+    
+    public void init() {
+        /*
+        // Registering EPSG:2056 / LV95 reference frame.
+        Proj4.defs(EPSG_2056, "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs");
+        ol.proj.Proj4.register(Proj4.get());
+
+        ProjectionOptions projectionOptions = OLFactory.createOptions();
+        projectionOptions.setCode(EPSG_2056);
+        projectionOptions.setUnits("m");
+        projectionOptions.setExtent(new Extent(2420000, 1030000, 2900000, 1350000));
+        projection = new Projection(projectionOptions);
+        Projection.addProjection(projection);
+        */
+        
+        // Change Domino UI color scheme.
+        Theme theme = new Theme(ColorScheme.RED);
+        theme.apply();
+
+        body().add(div().id(MAP_DIV_ID));
+        map = MapPresets.getBlakeAndWhiteMap(MAP_DIV_ID);
+    }    
+}
---
+++ somaps-server/src/test/java/ch/so/agi/somaps/somaps/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.so.agi.somaps.somaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
